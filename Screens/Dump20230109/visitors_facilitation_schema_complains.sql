c959d11a907c0fce6d842bc4d49f79ba7fd14fe9
michaelmungaimaina <113831324+michaelmungaimaina@users.noreply.github.com>
2023-01-09 23:43:31 +0300

    Add files via upload
    Screenshots for various screen views of the project

---
+++ Screens/Dump20230109/visitors_facilitation_schema_complains.sql
@@ -0,0 +1,63 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: visitors_facilitation_schema
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `complains`
+--
+
+DROP TABLE IF EXISTS `complains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `complains` (
+  `idcomplains` int NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `fname` varchar(25) DEFAULT NULL,
+  `sname` varchar(25) DEFAULT NULL,
+  `tname` varchar(25) DEFAULT NULL,
+  `idno` int DEFAULT NULL,
+  `krapin` varchar(25) DEFAULT NULL,
+  `dept` varchar(30) DEFAULT NULL,
+  `complain` varchar(2500) NOT NULL,
+  `report` varchar(2500) DEFAULT NULL,
+  `action` varchar(500) DEFAULT NULL,
+  `status` varchar(25) DEFAULT NULL,
+  `res_date` varchar(15) DEFAULT NULL,
+  `phone` int DEFAULT NULL,
+  PRIMARY KEY (`idcomplains`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `complains`
+--
+
+LOCK TABLES `complains` WRITE;
+/*!40000 ALTER TABLE `complains` DISABLE KEYS */;
+INSERT INTO `complains` VALUES (1,'2023-01-09','MICHAEL','MUNGAI','MAINA',36783358,'A012196678A','CUSTOMS & BORDER CONTROL','I would like to compliment about the works of this programmer. Indeed he is good!','COMPLIMENT','','','0000-00-00',711106986);
+/*!40000 ALTER TABLE `complains` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-09 20:12:38
